610b6e9cbebea74e62696112aece4dfa3ab722bc
Chase McCoy <chasem000@gmail.com>
2021-04-03 13:33:38 -0500

    ~

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,16 +1,14 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
 
 class MyDocument extends Document {
-  render() {
-    const shouldRenderScripts = process.env.NODE_ENV !== 'production'
-    
+  render() {    
     return (
       <Html lang="en">
         <Head />
         <body>
           <Main />
-          {shouldRenderScripts && <NextScript />}
-
+          <NextScript />
+          
           <script
 						async
 						src="https://platform.twitter.com/widgets.js"
