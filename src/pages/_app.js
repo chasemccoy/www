bc0122e90cc800d1e730ff0d44717f706ff78585
Chase McCoy <chasem000@gmail.com>
2021-04-14 12:27:27 -0500

    ~

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -6,12 +6,131 @@ import Metadata from '../components/Metadata';
 import Link from '../components/Link';
 import '../components/Logo'
 
+const fontStyles = `
+@font-face {
+  font-family: 'GT America';
+  font-style: normal;
+  font-weight: normal;
+  font-display: swap;
+  src: url('/fonts/gt-america/GTAmerica-Regular.woff2')
+      format('woff2'),
+    url('/fonts/gt-america/GTAmerica-Regular.woff')
+      format('woff');
+}
+
+@font-face {
+  font-family: 'GT America';
+  font-style: italic;
+  font-weight: normal;
+  font-display: swap;
+  src: url('/fonts/gt-america/GTAmerica-RegularItalic.woff2')
+      format('woff2'),
+    url('/fonts/gt-america/GTAmerica-RegularItalic.woff')
+      format('woff');
+}
+
+@font-face {
+  font-family: 'GT America';
+  font-style: normal;
+  font-weight: bold;
+  font-display: swap;
+  src: url('/fonts/gt-america/GTAmerica-Bold.woff2')
+      format('woff2'),
+    url('/fonts/gt-america/GTAmerica-Bold.woff')
+      format('woff');
+}
+
+@font-face {
+  font-family: 'GT America';
+  font-style: italic;
+  font-weight: bold;
+  font-display: swap;
+  src: url('/fonts/gt-america/GTAmerica-BoldItalic.woff2')
+      format('woff2'),
+    url('/fonts/gt-america/GTAmerica-BoldItalic.woff')
+      format('woff');
+}
+
+@font-face {
+  font-family: 'Ivar Text';
+  src: url('/fonts/ivar-text/IvarText-Regular.woff2') format('woff2'),
+    url('/fonts/ivar-text/IvarText-Regular.woff') format('woff');
+  font-weight: normal;
+  font-style: normal;
+  font-display: swap;
+}
+
+@font-face {
+  font-family: 'Ivar Text';
+  src: url('/fonts/ivar-text/IvarText-Italic.woff2') format('woff2'),
+    url('/fonts/ivar-text/IvarText-Italic.woff') format('woff');
+  font-weight: normal;
+  font-style: italic;
+  font-display: swap;
+}
+
+@font-face {
+  font-family: 'Ivar Text';
+  src: url('/fonts/ivar-text/IvarText-Medium.woff2') format('woff2'),
+    url('/fonts/ivar-text/IvarText-Medium.woff') format('woff');
+  font-weight: bold;
+  font-style: normal;
+  font-display: swap;
+}
+
+@font-face {
+  font-family: 'Ivar Text';
+  src: url('/fonts/ivar-text/IvarText-MediumItalic.woff2') format('woff2'),
+    url('/fonts/ivar-text/IvarText-MediumItalic.woff') format('woff');
+  font-weight: bold;
+  font-style: italic;
+  font-display: swap;
+}
+
+@font-face {
+  font-family: 'Source Code Pro';
+  src: url('/fonts/source-code-pro/SourceCodePro-It.woff2') format('woff2'),
+      url('/fonts/source-code-pro/SourceCodePro-It.woff') format('woff');
+  font-weight: normal;
+  font-style: italic;
+  font-display: swap;
+}
+
+@font-face {
+  font-family: 'Source Code Pro';
+  src: url('/fonts/source-code-pro/SourceCodePro-BoldIt.woff2') format('woff2'),
+      url('/fonts/source-code-pro/SourceCodePro-BoldIt.woff') format('woff');
+  font-weight: bold;
+  font-style: italic;
+  font-display: swap;
+}
+
+@font-face {
+  font-family: 'Source Code Pro';
+  src: url('/fonts/source-code-pro/SourceCodePro-Bold.woff2') format('woff2'),
+      url('/fonts/source-code-pro/SourceCodePro-Bold.woff') format('woff');
+  font-weight: bold;
+  font-style: normal;
+  font-display: swap;
+}
+
+@font-face {
+  font-family: 'Source Code Pro';
+  src: url('/fonts/source-code-pro/SourceCodePro-Regular.woff2') format('woff2'),
+      url('/fonts/source-code-pro/SourceCodePro-Regular.woff') format('woff');
+  font-weight: normal;
+  font-style: normal;
+  font-display: swap;
+}
+`
+
 const App = ({ Component, pageProps }) => {
   return (
     <div id="wrapper">
       <Head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <style dangerouslySetInnerHTML={{__html: fontStyles }} />
         <link rel="stylesheet" href="/styles/shared.css" />
         <link
           rel="alternate"
