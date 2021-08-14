59328804445e79001d4c67a314cc8a5bf545b66c
Chase McCoy <chasem000@gmail.com>
2021-08-14 12:54:13 -0500

    ~

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,11 +1,20 @@
 import React from 'react'
+import Markdown from 'react-markdown'
 
 const Image = (props) => {
   if (props.title) {
     return (
       <figure>
         <img {...props} />
-        <figcaption className='image-caption sans'>{props.title}</figcaption>
+        <figcaption className='image-caption sans'>
+          <Markdown
+            disallowedElements={['p']}
+            unwrapDisallowed
+            linkTarget='_blank'
+          >
+            {props.title}
+          </Markdown>
+        </figcaption>
       </figure>
     )
   }
