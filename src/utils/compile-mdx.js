e15dc4d5b67d4d494004ad8cd86a61d92f6b2c2d
Chase McCoy <chsmc@stripe.com>
2022-04-27 18:53:35 -0500

    update deps

--- src/utils/compile-mdx.js
+++ src/utils/compile-mdx.js
@@ -72,7 +72,7 @@ async function compileMdx(path, slug) {
   const { frontmatter, code } = await bundleMDX({
     file: path,
     cwd: nodePath.resolve(path, '..'),
-    xdmOptions(options) {
+    mdxOptions(options) {
       options.remarkPlugins = [
         ...(options.remarkPlugins ?? []),
         ...remarkPlugins,
