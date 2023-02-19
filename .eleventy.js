e53efbf9f61e5449a2baed3dc90a0ad1db5abd0a
Chase McCoy <chasem000@gmail.com>
2023-02-19 12:02:39 -0600

    ~

--- .eleventy.js
+++ .eleventy.js
@@ -121,28 +121,6 @@ module.exports = function (config) {
 
   config.setLibrary('md', markdownLibrary)
 
-  // Override Browsersync defaults (used only with --serve)
-  config.setBrowserSyncConfig({
-    callbacks: {
-      ready: function (err, browserSync) {
-        const content_404 = fs.readFileSync('_site/404.html')
-
-        browserSync.addMiddleware('*', (req, res) => {
-          // Provides the 404 content without redirect.
-          res.writeHead(404, { 'Content-Type': 'text/html; charset=UTF-8' })
-          res.write(content_404)
-          res.end()
-        })
-      },
-    },
-    ui: false,
-    ghostMode: false,
-    files: './src/_includes/styles/**/*.css',
-    snippetOptions: {
-      ignorePaths: 'faulkner/**',
-    },
-  })
-
   return {
     templateFormats: ['md', 'njk', 'html'],
     markdownTemplateEngine: 'njk',
