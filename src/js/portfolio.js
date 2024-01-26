7b5d7ea69ca6b6858199a2c67156c07c3fc52130
Chase McCoy <chasem000@gmail.com>
2024-01-26 11:48:58 -0600

    portfolio stuff

---
+++ src/js/portfolio.js
@@ -0,0 +1,31 @@
+const App = {}
+
+document.addEventListener('DOMContentLoaded', () => {
+  App.fixedHeader = document.getElementById('fixed-header')
+  App.scrollHeader = document.getElementById('scroll-header')
+
+  App.headerIntersectionCallback = (entries, observer) => {
+    const currentEntry = entries[0]
+
+    if (!currentEntry) return
+
+    if (currentEntry.isIntersecting) {
+      console.log('moved onscreen')
+      App.scrollHeader.classList.add('hidden')
+    } else {
+      console.log('moved offscreen')
+      App.scrollHeader.classList.remove('hidden')
+    }
+
+    document.documentElement.style.setProperty(
+      '--scroll-header-height',
+      App.scrollHeader.offsetHeight + 'px'
+    )
+  }
+
+  App.observer = new IntersectionObserver(App.headerIntersectionCallback)
+
+  App.observer.observe(App.fixedHeader)
+
+  console.log(App)
+})
