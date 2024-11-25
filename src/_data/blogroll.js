68e76775765ea6cfb31e4df9699b384a3da7c1aa
Chase McCoy <chasem000@gmail.com>
2024-11-25 08:33:52 -0600

    ~

--- src/_data/blogroll.js
+++ src/_data/blogroll.js
@@ -3,7 +3,7 @@ module.exports = [
   { name: 'Amy Hupe', url: 'https://amyhupe.co.uk' },
   { name: 'anhvn', url: 'https://anhvn.com' },
   { name: 'arcana dot computer', url: 'https://arcana.computer' },
-  { name: 'Audacious Fox', url: 'https://audaciousfox.net' },
+  { name: 'Audacious Fox', url: 'https://audacious.blog' },
   { name: 'Chen Hui Jing', url: 'https://chenhuijing.com' },
   { name: 'Chris Coyier', url: 'https://chriscoyier.net' },
   { name: 'Frank Chimero', url: 'https://frankchimero.com' },
